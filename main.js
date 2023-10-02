f1deff0ed8e6d34c8dd1ecf20a1a13ab6860f595
MPBCoder <xmpbgamerx@gmail.com>
2023-10-02 15:06:30 +0330

    added config files

---
+++ main.js
@@ -0,0 +1,8 @@
+import "sanitize.css";
+import "sanitize.css/system-ui.css";
+import "sanitize.css/ui-monospace.css";
+import "sanitize.css/forms.css";
+import "sanitize.css/assets.css";
+import "sanitize.css/typography.css";
+import "sanitize.css/reduce-motion.css";
+import "./style.css";
